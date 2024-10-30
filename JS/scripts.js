3517991b8b265f152f6dad6d04d062e471292415
Kuanysh <tkuanys767@gmail.com>
2024-10-31 00:07:32 +0600

    home work: Vareables and date tupes

---
+++ JS/scripts.js
@@ -0,0 +1,10 @@
+const studentName='Али',
+age=25,
+isStudent=true,
+ageInStr ='25'
+console.log(`Аты: ${studentName} | типі: ${typeof studentName}`)
+console.log(`Жасы ${age} | типі: ${typeof age}`)
+console.log(`Студент пе ${isStudent} | типі: ${typeof isStudent}`)
+console.log(`Жасы жол ретінде ${ageInStr} | типі: ${typeof ageInStr}`)
+
+
