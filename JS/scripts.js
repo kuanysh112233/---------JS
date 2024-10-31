9cc7c0b069ba607e679e91b2f54ba594110dbc28
Kuanysh <tkuanys767@gmail.com>
2024-11-01 00:27:37 +0600

    home word: OPERATIONS AND EXPRESSIONS

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,10 +1,6 @@
-const studentName='Али',
-age=25,
-isStudent=true,
-ageInStr ='25'
-console.log(`Аты: ${studentName} | типі: ${typeof studentName}`)
-console.log(`Жасы ${age} | типі: ${typeof age}`)
-console.log(`Студент пе ${isStudent} | типі: ${typeof isStudent}`)
-console.log(`Жасы жол ретінде ${ageInStr} | типі: ${typeof ageInStr}`)
 
-
+const a =Number( prompt('Бірінші санды жазыңыз')) , 
+b = Number(prompt('Екінші санды жазыңыз')), 
+c = Number(prompt('Үшінші санды жазыңыз'))
+const sum = ((a+b+c)/3)
+alert("Арифметикалық орта:" + (sum))
