93f616d63e340ee5388b750bf0543d15d0bb6677
Kuanysh <tkuanys767@gmail.com>
2024-11-05 00:29:49 +0600

    Operators and Expressions

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,6 +1,33 @@
 
-const a =Number( prompt('Бірінші санды жазыңыз')) , 
-b = Number(prompt('Екінші санды жазыңыз')), 
-c = Number(prompt('Үшінші санды жазыңыз'))
-const sum = ((a+b+c)/3)
-alert("Арифметикалық орта:" + (sum))
+
+//  1 задача
+// let age = 70
+// let isStudent = false
+// if(age>=65 || isStudent === true){
+//     alert('Сізге жеңілдік бар')
+// }
+// else{
+//     alert('Сізге жеңілдік Жоқ')
+// }
+
+// 2 задача
+// let num=Number(prompt("санды енгіз"))
+// if(10<=num && num<=20){
+//     alert(true)
+// }
+// else{
+//     alert(false)
+// }
+
+// 3 задача
+// let num1=-5
+// let num2=10
+// if(num1<0 || num2<0){
+//     alert(true)
+// }
+// if((num1<0 && num2<0) || (num1>0 && num2>0)){
+//     alert(false)
+// }
+
+
+
