785f5fb4ff432f7ccc84ef0b5d3a728c355acb05
Kuanysh <tkuanys767@gmail.com>
2024-11-14 08:49:12 +0600

    hw-11 week while do while

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,16 +1,34 @@
+// үй жумысы \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-    // Үйжумысы
+// for(let mumkindik=3; mumkindik!=0; mumkindik--){
+//     let soz = prompt("ҚАЗАҚ ХАНДЫҒЫ ҚАШАН ҚҰРЫЛДЫ  ");
+//     if(soz==="1465"){
+//         alert("siz durys jayap berdiniz");
+//          break;
+//     }
+//     if(mumkindik!==1){
+//         alert("cizde mymkindik azaidy: " + (mumkindik-1));
+//     }
+//     else{
+//         alert("cizde mymkindik kalmady");
+//         break;
+//     }
+// }
 
-    let a = +prompt("санды енгініз")
-    for(let i = 1; i<=a; i++){
-        if(i%3===0 && i%5===0){
-            console.log(i)
-        }
-        if(i%3===0 && i%5===0 && i%2===0){
+const allStudentCount = +prompt("студенттердің санын енгізіңіз");
+let i  = allStudentCount
+let pointSum = 0
+while(i!==0){
+    const studentPoint = +prompt(i+' Студенттің баллы:')
+    pointSum = pointSum + studentPoint
+    i--
+}
+alert('Орташа баға: '+ (pointSum/allStudentCount))
 
-            break;
-        }
+
+
+        
     
-    }
-    
+
+
 
