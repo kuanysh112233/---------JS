f47fdc284d94620837140056758749934caacf0d
Kuanysh <tkuanys767@gmail.com>
2024-11-21 09:25:57 +0600

    HW-12week-Advanced Function

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,33 +1,27 @@
 
-// Үй Жумысы \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-// let a = prompt("Студенттін атты");
-// let b = prompt("Студент қай тілде сөйлейді: english / kazakh / russian");
+const switchToEuro = (value,moneyType)=>{
+    console.log(moneyType.toLocaleUpperCase())
+    if(moneyType.toLocaleUpperCase() ==="USA"){
+        return value * 0.85
+    }
+    else if(moneyType.toLocaleUpperCase() ==="KZT"){
+        return value * 0.002
+    }
+    else{return value}
+}
 
-// function studentLanguages(a, b){
-//     if(b ==  "english"){
-//         return a + " HELLO! HOW ARE YOU!";
-//     }
-//     else if(b ==  "kazakh"){
-//         return a + "СӘЛЕМ ! ҚАЛАЙСЫН";
-//     }
-//     else if(b ==  "russian"){
-//         return a + "ПРИВЕТ! КАК ДЕЛА";
-//     }
-//     else{
-//         alert("Ондай студент и тіл жоқ!")
-//     }
-// }
-// alert(studentLanguages(a, b));
+let sum = 0
+let valutaSum = null
+let valuta =''
 
+while(true) {
+    valutaSum = +prompt("Ақша суммасын енгіз")
+    if(valutaSum===0){break;}
+    valuta = prompt("Валютаны енгіз")
+    if(valuta==='stop'){break;}
+    console.log(valutaSum,valuta)
+   sum = sum + switchToEuro(valutaSum,valuta)
+}
 
-
-
-
-
-
-
-
-
-
-
+alert(sum)
