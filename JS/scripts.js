d3b1f9b4aaf5a6ebd7f6948bf51eded6f49e502e
Kuanysh <tkuanys767@gmail.com>
2024-11-29 01:13:46 +0600

    HW-13WEEK- Iterating over Arrays

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,23 +1,40 @@
 
 
-// үй жымысы\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+// Үй жумыс\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-let arrou =[]
+// 1 задание
 
-while(true){
-    let soz = +prompt("San engiz")
-
-    if(soz===0){
-        break;
-    }
-    else if(soz%2===0){
-        arrou.unshift(soz)
-    }
-    else{
-        arrou.push(soz)
+let number = [1 , 2 , 3 , 4 , 5]
+alert("Бастапқы массивті сандар:   " + number)
+let triple =number.map(num => num * num * num);
+alert("Сандардын кубтанганнан кейің:   " + triple)
+for (let num of triple) {
+    if(num%2!==0){
+        console.log("Кубталған сандардын ішіндегі тақ сандар:  " + num)
     }
 }
-console.log(arrou);
+
+// 2 задание 
+
+let isStudent = +prompt("Қанша студент бар? ")
+let studentGrades = []
+while(isStudent!==0){
+    const number = +prompt("Студентін бағасы:  ")
+    console.log("Студенттің бағасы:  " + number);
+    studentGrades.push(number)
+    isStudent-- 
+}
+const totalSum = studentGrades.reduce((total, num) => total + num , 0)
+console.log("Ортақ баға:  " + totalSum / studentGrades.length);
+const gradesMoreThan85 = studentGrades.filter(grade=>grade>=85)
+console.log("85 -тен жоғары бағалар:  " + gradesMoreThan85);
+
+
+
+
+ 
+
+
 
 
 
